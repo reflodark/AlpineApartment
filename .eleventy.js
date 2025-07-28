522db7111d76c947a107edc6434ef581b00f0513
reflodark <reflodark@proton.me>
2025-07-28 09:22:49 +0200

    Add source folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 module.exports = function (eleventyConfig) {
     // Passthrough-Copy für statische Assets. Eleventy kopiert diese Ordner/Dateien
     // direkt in den Output-Ordner (_site).
-    eleventyConfig.addPassthroughCopy("favicon.ico");
-    eleventyConfig.addPassthroughCopy("./images");
-    eleventyConfig.addPassthroughCopy("./styles.css");
-    eleventyConfig.addPassthroughCopy("./app.js");
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/styles.css");
+    eleventyConfig.addPassthroughCopy("src/app.js");
 
     return {
         // Definiert die Verzeichnisstruktur für Eleventy.
         dir: {
-            input: ".",
+            input: "src",
             includes: "_includes",
             output: "_site",
         },
